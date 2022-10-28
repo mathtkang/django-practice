9cf83ed847688c0da8e4e48e68afb07a49f28939
[2기-1반] 강석영 <ksge1124@gmail.com>
2022-10-29 00:47:45 +0900

    update board app

--- boards/models.py
+++ boards/models.py
@@ -26,3 +26,52 @@ class BoardTag(models.Model):
 
     board_id = models.ForeignKey(Board, on_delete=models.CASCADE)
     tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=150,
+    )
+    content = models.TextField(
+        blank=True,
+        default="",
+    )
+    written_user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    board_id = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+
+class Attachment(models.Model):
+    path = models.TextField(
+        primary_key=True,
+    )
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(
+        _("created at"),
+        default=timezone.now,
+    )
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    parent_comment_id = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,  # nullable
+    )
+
+
+class Like(models.Model):
+    """connect Post & User"""
+
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Mark(models.Model):
+    """connect Post & User"""
+
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
